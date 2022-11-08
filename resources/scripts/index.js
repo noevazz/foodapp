742c2345bb24ed2463cafef5d4f2b1e273c4a348
noevazz <jonovagu@gmail.com>
2022-11-07 21:00:36 -0600

    [ADD] added fetch

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -28,13 +28,26 @@ reviewsElement.innerHTML = amountOfReviews(110);
 
 
 let grads = 0;
+
+function updateDish() {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.meals[0].strMealThumb);
+        console.log(data.meals[0].strMeal);
+        document.getElementById('imageSalad').src = data.meals[0].strMealThumb;
+        document.querySelector('#title h1').innerHTML = data.meals[0].strMeal;
+    });
+}
 prevElement.addEventListener('click', ()=>{
     grads += 360;
     document.getElementById('imageSalad').style.transform = `rotate(${grads}deg)`;
+    updateDish();
 });
 nextElement.addEventListener('click', ()=>{
     grads -= 360;
     document.getElementById('imageSalad').style.transform = `rotate(${grads}deg)`;
+    updateDish();
 });
 
 removeItem.addEventListener('click', ()=>{
